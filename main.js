0654aa8e6e1c367f2827e31031742ca2d804c4bf
Josué <josue.jackson15@outlook.pt>
2024-05-09 16:04:24 +0100

    Changed and fininished

--- main.js
+++ main.js
@@ -2,18 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-if (req.query.name && req.query.age) {
-    const { name, age } = req.query;
-    res.send(`Hello ${name}(${age}), you are a Pawg! ºoºoº`);
-  } else if (req.query.name) {
-    res.send(`Hello FOCK ! ${req.query.name}`);
-  } else {
-    res.send("Hello World!");
-  }
+// app.get("/", (req, res) => {
+// if (req.query.name && req.query.age) {
+//     const { name, age } = req.query;
+//     res.send(`Hello ${name}(${age}), you are a Pawg! ºoºoº`);
+//   } else if (req.query.name) {
+//     res.send(`Hello FOCK ! ${req.query.name}`);
+//   } else {
+//     res.send("Hello World!");
+//   }
+// });
+
+app.post("/api/forum/:name/:age/:height", (req, res) => {
+  const { name, age, height } = req.params;
+  res.send(`Hello ${name}(${age}), you are a Pawg! ºoºoº`);
+  console.log(req.params);
+  const dados = [];
+  dados.push({
+    name: name,
+    age: age,
+    height: height,
+    data: new Date().toLocaleTimeString(),
+  });
+  res.send(dados);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
